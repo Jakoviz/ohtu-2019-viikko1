db98c0cea39f59659ae6bf687d1f4e7dce1f95bb
Jaakko Paavola <jakoviz@gmail.com>
2019-11-04 22:36:27 +0200

    Yhtä testiä muutettu ei-läpimeneväksi.

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -74,7 +74,7 @@ public class VarastoTest {
 
     @Test
     public void merkkijonoEsitysVarastoOliolle() {
-	    assertEquals("saldo = 0.0, vielä tilaa 10.0", varasto.toString());
+	    assertEquals("saldo = 0, vielä tilaa 10.0", varasto.toString());
     }
 
     @Test
